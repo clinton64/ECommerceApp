3df92b157fe438c638e673e106306e8a2fbac359
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-09-22 20:17:35 +0200

    configure DB

---
+++ ECommerceApp.Services.CouponService/Data/AppDBContext.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceApp.Services.CouponService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Services.CouponService.Data;
+
+public class AppDBContext : DbContext
+{
+	public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+	{
+		
+	}
+
+	public DbSet<Coupon> Coupons { get; set; }	
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+
+		/*modelBuilder.Entity<Coupon>().HasData(
+			new Coupon
+			{
+				Id = 1,
+				CouponCode = "10OFF",
+				DiscountAmount = 10,
+				MinimumAmount = 50
+			},
+			new Coupon
+			{
+				Id = 2,
+				CouponCode = "20OFF",
+				DiscountAmount = 20,
+				MinimumAmount = 100
+			}
+		);*/
+	}
+}
