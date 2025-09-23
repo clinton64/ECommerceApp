80b8ee4dda876e3e25ba7897a18117b014347204
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-09-23 16:38:25 +0200

    coupon service integration

---
+++ ECommerceApp.Web/Service/CouponService.cs
@@ -0,0 +1,69 @@
+﻿using ECommerceApp.Web.Models;
+using ECommerceApp.Web.Service.IService;
+using static ECommerceApp.Web.Utility.StaticData;
+
+namespace ECommerceApp.Web.Service;
+
+public class CouponService : ICouponService
+{
+	private readonly IBaseService _baseService;
+	public CouponService(IBaseService baseService)
+	{
+		_baseService = baseService;
+	}
+	public async Task<ResponseDto?> CreateCouponAsync(CouponDto couponCreateDto)
+	{
+		return await _baseService.SendAsync(new RequestDto
+		{
+			ApiType= ApiType.POST,
+			Url = CouponAPIBase + "/api/coupon",
+			Data = couponCreateDto
+		});
+	}
+
+	public async Task<ResponseDto?> DeleteCouponAsync(int id)
+	{
+		return await _baseService.SendAsync(new RequestDto
+		{
+			ApiType = ApiType.DELETE,
+			Url = CouponAPIBase + "/api/coupon/" + id,
+		});
+	}
+
+	public async Task<ResponseDto?> GetAllCouponsAsycn()
+	{
+		return await _baseService.SendAsync(new RequestDto
+		{
+			ApiType = ApiType.GET,
+			Url = CouponAPIBase + "/api/coupon",
+		});	
+	}
+
+	public async Task<ResponseDto?> GetCouponAsync(string couponCode)
+	{
+		return await _baseService.SendAsync(new RequestDto
+		{
+			ApiType = ApiType.GET,
+			Url = CouponAPIBase + "/api/coupon/GetByCode/" + couponCode
+		});
+	}
+
+	public async Task<ResponseDto?> GetCouponAsync(int id)
+	{
+		return await _baseService.SendAsync(new RequestDto
+		{
+			ApiType = ApiType.GET,
+			Url = CouponAPIBase + "/api/coupon/" + id
+		});
+	}
+
+	public async Task<ResponseDto?> UpdateCouponAsync(CouponDto couponUpdateDto)
+	{
+		return await _baseService.SendAsync(new RequestDto
+		{
+			ApiType = ApiType.PUT,
+			Url = CouponAPIBase + "/api/coupon",
+			Data = couponUpdateDto
+		});
+	}
+}
---
+++ ECommerceApp.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceApp.Web.Models;
+
+namespace ECommerceApp.Web.Service.IService;
+
+public interface ICouponService
+{
+	Task<ResponseDto?> GetAllCouponsAsycn();
+	Task<ResponseDto?> GetCouponAsync(string couponCode);
+	Task<ResponseDto?> GetCouponAsync(int id);
+	Task<ResponseDto?> CreateCouponAsync(CouponDto couponCreateDto);
+	Task<ResponseDto?> UpdateCouponAsync(CouponDto couponUpdateDto);
+	Task<ResponseDto?> DeleteCouponAsync(int id);
+}
