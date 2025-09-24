98d71d8ec4423caded93085165cbdefaafb2926c
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-09-25 01:02:09 +0200

    Auth service IdentityDB configure

---
+++ ECommerceApp.Services.AuthService/Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Services.AuthService.Dara;
+
+public class AppDBContext : IdentityDbContext<IdentityUser>
+{
+	public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.OnModelCreating(builder);
+	}
+}
--- ECommerceApp.Services.AuthService/Program.cs
+++ ECommerceApp.Services.AuthService/Program.cs
@@ -1,8 +1,18 @@
+using ECommerceApp.Services.AuthService.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+	.AddEntityFrameworkStores<AppDBContext>()
+	.AddDefaultTokenProviders();
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
