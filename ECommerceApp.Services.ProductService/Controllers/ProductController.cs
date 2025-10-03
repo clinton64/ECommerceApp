4ec8651160a96acb19d66520399df51bb90546f5
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-04 01:08:56 +0200

    product controller

---
+++ ECommerceApp.Services.ProductService/Controllers/ProductController.cs
@@ -0,0 +1,168 @@
+﻿using AutoMapper;
+using ECommerceApp.Services.ProductService.Data;
+using ECommerceApp.Services.ProductService.Models;
+using ECommerceApp.Services.ProductService.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Services.ProductService.Controllers
+{
+	[Route("api/Product")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+		private readonly IMapper _mapper;
+		private ResponseDto _response;
+
+		public ProductController(AppDbContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+			_response = new ResponseDto();	
+		}
+
+		[HttpGet]
+		public ResponseDto Get()
+		{
+			try
+			{
+				var objList = _context.Products.ToList();
+				_response.Result = _mapper.Map<List<ProductDto>>(objList);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpGet]
+		[Route("{id:int}")]
+		public ResponseDto Get(int id)
+		{
+			try
+			{
+				var obj = _context.Products.First(u => u.Id == id);
+				_response.Result = _mapper.Map<ProductDto>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpPost]
+		public ResponseDto Post([FromForm] ProductDto productDto)
+		{
+			try
+			{
+				var product = _mapper.Map<Product>(productDto);
+				_context.Products.Add(product);
+				_context.SaveChanges();
+
+				if(productDto.Image != null)
+				{
+					string fileName = product.Id + Path.GetExtension(productDto.Image.FileName);
+					var filePath = @"\wwwroot\ProductImages\" + fileName;
+
+					// Delete if any existing file
+					var directory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+					var existingFile = new FileInfo(directory);
+					if (existingFile.Exists)
+					{
+						existingFile.Delete();
+					}
+					using (var stream = new FileStream(directory, FileMode.Create))
+					{
+						productDto.Image.CopyTo(stream);
+					}
+
+					var baseUrl = $"{this.Request.Scheme}://{this.Request.Host.Value}{this.Request.PathBase.Value}";
+					product.ImageUrl = baseUrl + "/ProductImages/" + fileName;
+					product.ImageLocalPath = filePath;
+				}
+				_context.Products.Update(product);
+				_context.SaveChanges();
+
+				_response.Result = _mapper.Map<ProductDto>(product);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpPut]
+		public ResponseDto Put([FromForm] ProductDto productDto)
+		{
+			try
+			{
+				var product = _mapper.Map<Product>(productDto);
+
+				if (productDto.Image != null)
+				{
+					string fileName = product.Id + Path.GetExtension(productDto.Image.FileName);
+					var filePath = @"\wwwroot\ProductImages\" + fileName;
+
+					// Delete if any existing file
+					var directory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+					var existingFile = new FileInfo(directory);
+					if (existingFile.Exists)
+					{
+						existingFile.Delete();
+					}
+					using (var stream = new FileStream(directory, FileMode.Create))
+					{
+						productDto.Image.CopyTo(stream);
+					}
+
+					var baseUrl = $"{this.Request.Scheme}://{this.Request.Host.Value}{this.Request.PathBase.Value}";
+					product.ImageUrl = baseUrl + "/ProductImages/" + fileName;
+					product.ImageLocalPath = filePath;
+				}
+				_context.Products.Update(product);
+				_context.SaveChanges();
+
+				_response.Result = _mapper.Map<ProductDto>(product);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpDelete]
+		[Route("{id:int}")]
+		public ResponseDto Delete(int id)
+		{
+			try
+			{
+				var obj = _context.Products.First(u => u.Id == id);
+				if(! string.IsNullOrEmpty(obj.ImageLocalPath))
+				{
+					var directory = Path.Combine(Directory.GetCurrentDirectory(), obj.ImageLocalPath);
+					var existingFile = new FileInfo(directory);
+					if (existingFile.Exists)
+					{
+						existingFile.Delete();
+					}
+				}
+				_context.Products.Remove(obj);
+				_context.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+	}
+}
---
+++ ECommerceApp.Services.ProductService/Profiles/ProductProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ECommerceApp.Services.ProductService.Models;
+using ECommerceApp.Services.ProductService.Models.DTO;
+
+namespace ECommerceApp.Services.CouponService.Profiles;
+
+public class ProductProfile : Profile
+{
+	public ProductProfile()
+	{
+		CreateMap<Product, ProductDto>().ReverseMap();
+	}
+}
