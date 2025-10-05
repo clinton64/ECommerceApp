b4804c21307a992e52637abefdce6daa5838e14b
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-05 19:11:15 +0200

    product imageUrls Nullable

--- ECommerceApp.Services.ProductService/Models/DTO/ProductDto.cs
+++ ECommerceApp.Services.ProductService/Models/DTO/ProductDto.cs
@@ -2,16 +2,16 @@
 
 public class ProductDto
 {
-
+	public int Id { get; set; }	
 	public string Name { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
 
 	public double Price { get; set; }
 	public string Category { get; set; } = string.Empty;
 
-	public string ImageUrl { get; set; } = string.Empty;
+	public string? ImageUrl { get; set; } = string.Empty;
 
-	public string ImageLocalPath { get; set; } = string.Empty;	
+	public string? ImageLocalPath { get; set; } = string.Empty;	
 
 	public IFormFile? Image { get; set; }
 }
--- ECommerceApp.Web/Models/ProductDto.cs
+++ ECommerceApp.Web/Models/ProductDto.cs
@@ -1,21 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace ECommerceApp.Web.Models;
 
-namespace ECommerceApp.Services.ProductService.Models;
-
-public class Product
+public class ProductDto
 {
-	[Key]
 	public int Id { get; set; }
-
-	[Required]
 	public string Name { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
 
-	[Range(1,1000)]
 	public double Price { get; set; }
 	public string Category { get; set; } = string.Empty;
 
 	public string? ImageUrl { get; set; } = string.Empty;
 
 	public string? ImageLocalPath { get; set; } = string.Empty;
+
+	public IFormFile? Image { get; set; }
 }
