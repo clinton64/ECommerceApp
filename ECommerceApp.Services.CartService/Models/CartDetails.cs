7bef15eb84388a1695f8c230b2714a79115916ac
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-10 11:44:14 +0200

    Add DB models

---
+++ ECommerceApp.Services.CartService/Models/CartDetails.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApp.Services.CartService.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceApp.Services.CartService.Models;
+
+public class CartDetails
+{
+	[Key]
+	public int Id { get; set; }
+
+	public int CartHeaderId{ get; set; }
+	[ForeignKey("CartHeaderId")]
+	public CartHeader CartHeader { get; set; }
+	
+	public int ProductId{ get; set; }
+	[NotMapped]
+	public ProductDto Product { get; set; }
+	public int Count { get; set; }	
+}
