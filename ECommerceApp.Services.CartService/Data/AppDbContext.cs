da1a02bb01c180292d44aaa5145397fe463ea935
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-10 11:48:27 +0200

    DB migration

---
+++ ECommerceApp.Services.CartService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceApp.Services.CartService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Services.CartService.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+		
+	}
+
+	public DbSet<CartHeader> CartHeaders { get; set; }
+	public DbSet<CartDetails> CartDetails { get; set; }	
+}
