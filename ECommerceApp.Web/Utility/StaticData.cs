1dc3ba7eba9e395378672a279ad82d943603c8c6
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-14 00:36:35 +0200

    cart Navigation

--- ECommerceApp.Web/Utility/StaticData.cs
+++ ECommerceApp.Web/Utility/StaticData.cs
@@ -7,6 +7,8 @@ public static class StaticData
 	public static string ProductAPIBase = "https://localhost:7003";
 	public static string CartAPIBase = "https://localhost:7004";
 	public const string TokenCookie = "JWTToken";
+	public const string Role_User_Admin = "Admin";
+
 	public enum ApiType
 	{
 		GET,
