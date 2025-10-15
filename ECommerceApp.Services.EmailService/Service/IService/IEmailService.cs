c426183f9019604f3536fbadfdbf5482fe47b028
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-15 22:47:25 +0200

    Email Service

---
+++ ECommerceApp.Services.EmailService/Service/IService/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceApp.Services.EmailService.Models;
+using ECommerceApp.Services.EmailService.Models.DTO;
+
+namespace ECommerceApp.Services.EmailService.Service.IService;
+
+public interface IEmailService
+{
+    Task EmailCartAndLog(CartDto cartDto);
+    Task RegisterUserEmailAndLog(string email);
+    Task LogOrderPlaced(RewardsMessage rewardsDto);
+}
