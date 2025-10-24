27693c89dffa31e2b4efe85febb4a23bf71bf416
clinton64 <clintonchakma.24csedu.064@gmail.com>
2025-10-24 10:49:53 +0200

    clean Order service

---
+++ ECommerceApp.Services.OrderService/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Services.OrderService.Controllers;
+
+[Route("api/order")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+}
